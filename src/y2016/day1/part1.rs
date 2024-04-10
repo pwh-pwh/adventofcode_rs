4ddfd284255175068f1ee95fb06be56c0f05713c
pwh-pwh <plmoknpwh@163.com>
2024-04-10 20:58:18 +0800

    add input file

--- src/y2016/day1/part1.rs
+++ src/y2016/day1/part1.rs
@@ -1,13 +1,19 @@
+use std::fs::File;
+use std::io::Read;
+
 fn sayHi() {
+    let mut ct = String::new();
+    let _ = File::open("src/y2016/day1/input.txt").unwrap().read_to_string(&mut ct);
+    println!("{ct}");
     println!("hello");
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn test() {
         sayHi();
     }
-
 }
