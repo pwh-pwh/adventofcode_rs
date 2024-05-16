19372116c7351175042e1582ffce9ee201473de3
pwh-pwh <plmoknpwh@163.com>
2024-05-16 17:52:18 +0800

    y2016 day1 init data

--- src/y2016/day1/part1.rs
+++ src/y2016/day1/part1.rs
@@ -1,12 +1,65 @@
 use std::fs::File;
 use std::io::Read;
+use std::str::FromStr;
+
+#[derive(Debug)]
+struct Go {
+    step: usize,
+    direction: Direction,
+}
+
+#[derive(Debug)]
+enum Direction {
+    Left,
+    Right,
+}
+
+type Location = (i32, i32);
+
+//方位azimuth
+#[derive(Debug)]
+enum Azimuth {
+    East,
+    West,
+    North,
+    South,
+}
+
+#[derive(Debug)]
+struct State {
+    location: Location,
+    azimuth: Azimuth,
+}
 
 fn sayHi() {
     let mut ct = String::new();
     let _ = File::open("src/y2016/day1/input.txt").unwrap().read_to_string(&mut ct);
-    println!("{ct}");
-    println!("hello");
+    let goList = ct.split(", ").map(|item| {
+        Go::from_str(item).unwrap()
+    }
+    ).collect::<Vec<Go>>();
+    println!("{goList:?}");
 }
+
+impl FromStr for Go {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let c1 = s.as_bytes()[0] as char;
+        let numStr = &s[1..].trim();
+        let dir = if c1 == 'R' {
+            Direction::Right
+        } else {
+            Direction::Left
+        };
+        let step = numStr.parse().unwrap();
+        Ok(Self {
+            step,
+            direction: dir,
+        })
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
