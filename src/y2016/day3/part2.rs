1dd67212cd0bf3efddc67c5edcbe6c0ce0025ef9
pwh-pwh <plmoknpwh@163.com>
2024-05-21 10:30:20 +0800

    y2016 day3 part2 impl

---
+++ src/y2016/day3/part2.rs
@@ -0,0 +1,39 @@
+fn get_triangle(s: &str) -> Vec<i32> {
+    return s.split_whitespace()
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+}
+
+
+fn part2() {
+    let input = include_str!("./input.txt");
+    let mut pos = 0;
+    let mut lines = input.lines();
+    loop {
+        match lines.next() {
+            Some(line1) => {
+                let line1 = get_triangle(line1);
+                let line2 = get_triangle(lines.next().unwrap());
+                let line3 = get_triangle(lines.next().unwrap());
+                for i in 0..3 {
+                    if line1[i] + line2[i] > line3[i] && line2[i] + line3[i] > line1[i] &&
+                        line3[i] + line1[i] > line2[i] {
+                        pos += 1;
+                    }
+                }
+            }
+            None => break,
+        }
+    }
+    println!("{}", pos);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::y2016::day3::part2::part2;
+
+    #[test]
+    fn test() {
+        part2();
+    }
+}
