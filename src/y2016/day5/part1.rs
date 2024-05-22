e746bc36f76e9edc3e4e01cff7fe13253f7ec46f
pwh-pwh <plmoknpwh@163.com>
2024-05-22 17:58:01 +0800

    2016 day5 part1 unimpl

--- src/y2016/day5/part1.rs
+++ src/y2016/day5/part1.rs
@@ -1,14 +1,15 @@
 use std::ops::Add;
+use md5::Digest;
 
 fn part1() {
     let input = "abc";
     let mut result = String::new();
     let mut num = 0;
     println!("part1");
-    for i in 0..8 {
+    for i in 0..2 {
         loop {
             let digest = md5::compute(format!("{input}{num}"));
-            if digest.starts_with(&[0;2]) {
+            if is_five_zero(digest) {
                 let d = format!("{:x}", (*digest.get(5).unwrap()));
                 result.push_str(&d);
                 println!("result debug: {result:?}");
@@ -22,6 +23,11 @@ fn part1() {
         }
     }
     println!("result: {result:?}");
+}
+
+fn is_five_zero(hash:Digest) -> bool {
+    let hexString = hash.to_vec();
+    hexString[0] == 0 && hexString[1] == 0 && hexString[2] == 0 && hexString[3] == 0 && hexString[4] == 0
 }
 
 #[cfg(test)]
