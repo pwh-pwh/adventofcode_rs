5b380c525bfa88fb4c3ba3904c222cb1c71e0fd0
pwh-pwh <plmoknpwh@163.com>
2024-05-27 17:54:04 +0800

    2016 day8 part1 init

--- src/y2016/day8/part1.rs
+++ src/y2016/day8/part1.rs
@@ -16,7 +16,32 @@ enum LineType {
 }
 
 fn part1() {
+    let input = include_str!("./input.txt");
+    let mut matrix = [[50;0];6];
+    let action_list:Vec<Action> = input.lines().map(|line| {
+        if line.starts_with("rect") {
+            parse_rect(line)
+        } else {
+            parse_roate(line)
+        }
+    }).collect();
+}
 
+fn parse_rect(line: &str) ->Action {
+    let ls = line.split(" ").nth(2).unwrap();
+    // todo get x y
+    Action::Rect {
+        x: 1,
+        y: 1
+    }
+}
+
+fn parse_roate(line: &str) -> Action {
+    Action::Roate {
+        line: 1,
+        step: 1,
+        line_type: LineType::Column,
+    }
 }
 
 #[cfg(test)]
