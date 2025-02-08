51b3d8e7de212e14ee07af3214c0412a6025f2c4
pwh-pwh <plmoknpwh@163.com>
2025-02-08 14:23:07 +0800

    2016 day19 part2 impl

--- src/y2016/day19/part1.rs
+++ src/y2016/day19/part1.rs
@@ -2,6 +2,10 @@ fn part1() {
     println!("{}", winning_elf(3012210));
 }
 
+fn part2() {
+    println!("{}", winning_elf2(3012210));
+}
+
 fn winning_elf(n: u32) -> u32 {
     // 计算不超过 n 的最大 2 的幂对应的指数 m
     let m = ((n as f64).log2().floor()) as u32;
@@ -13,12 +17,25 @@ fn winning_elf(n: u32) -> u32 {
     2 * L + 1
 }
 
+fn winning_elf2(n: u32) -> u32 {
+    let mut i = 1;
+    while i * 3 < n {
+        i *= 3;
+    }
+    n - i
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::y2016::day19::part1::part1;
+    use crate::y2016::day19::part1::{part1, part2};
 
     #[test]
-    fn test() {
+    fn test_part1() {
         part1();
+    }
+    
+    #[test]
+    fn test_part2() {
+        part2();
     }
 }
