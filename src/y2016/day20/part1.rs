a64e6d1cb91dfe02bf6fe5f08bdecce896cf6f2d
pwh-pwh <plmoknpwh@163.com>
2025-02-08 17:46:47 +0800

    2016 day20 part2 impl

--- src/y2016/day20/part1.rs
+++ src/y2016/day20/part1.rs
@@ -21,6 +21,27 @@ fn part1() {
     println!("{}", first_ip);
 }
 
+fn part2() {
+    let input = include_str!("./input.txt");
+    let mut ranges: Vec<(i64, i64)> = input.lines().map(|s| {
+        let narrs:Vec<_> = s.split("-").collect();
+        let start = narrs[0].parse::<i64>().unwrap();
+        let end = narrs[1].parse::<i64>().unwrap();
+        (start, end)
+    }).collect();
+    ranges.sort();
+    let mut next = 0;
+    let mut count = 0;
+    for (start,end) in ranges {
+        if start > next {
+            count += start - next - 1;
+        }
+        next = max(next, end);
+    }
+    count += 2i64.pow(32) - 1 - next;
+    println!("{}", count);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -28,5 +49,10 @@ mod tests {
     #[test]
     fn test_part1() {
         part1();
+    }
+
+    #[test]
+    fn test_part2() {
+        part2();
     }
 }
